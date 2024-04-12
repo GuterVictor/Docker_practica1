ef993b34a4a90fedf7ed8c0f850774d79b7f29be
Victor Paredes <83627662+GuterVictor@users.noreply.github.com>
2024-04-11 21:00:35 -0600

    Update script_master_p1.sh

--- scripts_k8/script_master_p1.sh
+++ scripts_k8/script_master_p1.sh
@@ -74,6 +74,8 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+kubeadm join k8-master:6443
+
 ########## Despues de las uniones de los workers #########
 
 kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.3/manifests/calico.yaml
